a77b2b5e117e8043d3599d926cb678d89dd9609b
Joao Marcelo Heusi <jmarcelo.heusi@gmail.com>
2022-01-09 11:01:14 -0300

    importando css

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const isDevelopment = process.env.NODE_ENV !== "production";
 
 module.exports = {
   mode: isDevelopment ? "development" : "production",
-  devtool: isDevelopment ? "inline-source-map" : "source-map",
+  devtool: isDevelopment ? "eval-source-map" : "source-map",
   entry: path.resolve(__dirname, "src", "index.jsx"),
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -13,6 +13,9 @@ module.exports = {
   },
   resolve: {
     extensions: [".js", ".jsx"],
+  },
+  devServer: {
+    static: path.resolve(__dirname, "public"),
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -25,6 +28,11 @@ module.exports = {
         test: /\.jsx$/,
         exclude: /node_modules/,
         use: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
