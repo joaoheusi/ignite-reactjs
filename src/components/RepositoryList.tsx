59bcd25b2df19ea0d29c49be6c4e005c337cf360
Joao Marcelo Heusi <jmarcelo.heusi@gmail.com>
2022-01-09 13:33:50 -0300

    migrando para typescript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -1,8 +1,15 @@
 import { RepositoryItem } from "./RepositoryItem";
 import { useState, useEffect } from "react";
 
+
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch("https://api.github.com/orgs/rocketseat/repos")
@@ -18,7 +25,7 @@ export function RepositoryList() {
 
       <ul>
         {repositories.map((repository) => {
-          return <RepositoryItem key={repository.id} repository={repository} />;
+          return <RepositoryItem key={repository.name} repository={repository} />;
         })}
       </ul>
     </section>
