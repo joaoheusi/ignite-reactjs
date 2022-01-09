98941dd10003d675b284309493487d12eb0a4f4f
Joao Marcelo Heusi <jmarcelo.heusi@gmail.com>
2022-01-09 12:28:14 -0300

    utilizando useEffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,4 +1,5 @@
 import { RepositoryItem } from "./RepositoryItem";
+import { useState, useEffect } from "react";
 
 const repository = {
   name: "unform",
@@ -6,7 +7,19 @@ const repository = {
   link: "https://github.com/unform/unform",
 };
 
+// https://api.github.com/orgs/rocketseat/repos
+
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/rocketseat/repos")
+      .then((response) => response.json())
+      .then((data) => {
+        setRepositories(data)
+      });
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
